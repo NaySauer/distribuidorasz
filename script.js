a908c4128b5ca13f35163a7398d063b1765249c3
NaySauer <100716950+NaySauer@users.noreply.github.com>
2026-06-04 23:52:52 -0300

    Update script.js
    productos

--- script.js
+++ script.js
@@ -1,3 +1,510 @@
+const productos = [
+
+    {
+        nombre: "Fideos al huevo",
+        precio: 1700,
+        imagen: "img/Fideos_al_huevo.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+
+    {
+        nombre: "Tallarínes de semolín",
+        precio: 1990,
+        imagen: "img/fideoSemolin.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Empanadas de hojaldre",
+        precio: 1400,
+        imagen: "img/empanadas.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Empanadas criollas",
+        descripcion: "aptas para horno y freir",
+        precio: 1240,
+        imagen: "img/empanadas-criollas.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Empanadas copetin",
+        descripcion: "24 unidades",
+        precio: 1900,
+        imagen: "img/copetin.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Tapas para pasteles",
+        descripcion: "24 unidades",
+        precio: 1750,
+        imagen: "img/pasteles.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Ravioles pasteurizados",
+        descripcion: "-4 quesos <br> -Pollo y espinaca <br> -Ricota",
+        precio: 2450,
+        imagen: "img/ravioles.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Ravioles de verdura 2 planchas",
+        descripcion: "Estilo caseros, 600gr",
+        precio: 2700,
+        imagen: "img/raviolesVerdura.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Ravioles x kilo",
+        descripcion: "-4 quesos <br> -Pollo y espinaca",
+        precio: 4250,
+        imagen: "img/raviolesKilo.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Pascualina Hojaldre",
+        precio: 1850,
+        imagen: "img/pascualinaHojaldre.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Pascualina Criolla",
+        precio: 1600,
+        imagen: "img/pascualinaCriolla.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Pascualina Criolla Oferta",
+        precio: 2050,
+        imagen: "img/pascualinaOferta.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Ñoquis de semolín",
+        precio: 1900,
+        imagen: "img/ñoquis.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Capellettis",
+        descripcion: "-4 quesos <br> -Pollo y espinaca",
+        precio: 2450,
+        imagen: "img/capellettis.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Sorrentinos",
+        descripcion: "de jamón y queso",
+        precio: 3350,
+        imagen: "img/sorrentinos.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Tortillas originales",
+        descripcion: "8 unidades",
+        precio: 2300,
+        imagen: "img/tortillas.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Pan Mil semillas",
+        precio: 3310,
+        imagen: "img/panSemillas.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Pan Lactal y Doble Salvado",
+        descripcion: "especificar en el pedido",
+        precio: 2100,
+        imagen: "img/panLactal.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Pre-pizzas",
+        descripcion: "dobles",
+        precio: 2550,
+        imagen: "img/pizza.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Pan para panchos",
+        descripcion: "6 unidades",
+        precio: 1600,
+        imagen: "img/panchos.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Super panchos",
+        descripcion: "6 unidades",
+        precio: 1900,
+        imagen: "img/superPancho.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Pan para hamburguesas",
+        descripcion: "6 unidades",
+        precio: 1600,
+        imagen: "img/hamburguesas.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+    {
+        nombre: "Talitas",
+        descripcion: "especificar en el pedido",
+        precio: 900,
+        imagen: "img/talitas.png",
+        categoria: "Pastas Quiero!: El mejor hojaldre del país"
+    },
+
+    {
+        nombre: "Leche Entera",
+        descripcion: "especifica en el pedido",
+        precio: 20400,
+        imagen: "img/leche.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Leche 0 Lactosa",
+        descripcion: "pack x 12",
+        precio: 23800,
+        imagen: "img/lecheDeslactosada.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Leche 0 Grasa",
+        descripcion: "pack x 12",
+        precio: 19500,
+        imagen: "img/leche0Grasa.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Chocolatada x litro",
+        descripcion: "pack x 12",
+        precio: 27200,
+        imagen: "img/chocolatadaLitro.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Chocolatada x 200",
+        descripcion: "pack x 12",
+        precio: 16200,
+        imagen: "img/chocolatada200.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Arroz con leche",
+        descripcion: "especificar en el pedido",
+        precio: 1150,
+        imagen: "img/ArrozconLeche.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Crema 200cc",
+        precio: 2050,
+        imagen: "img/crema200.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Crema 350cc",
+        precio: 3350,
+        imagen: "img/crema350.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Queso crema 190g",
+        descripcion: "especificar en el pedido",
+        precio: 1990,
+        imagen: "img/quesoCrema190.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Queso crema 280g",
+        descripcion: "especificar en el pedido",
+        precio: 2800,
+        imagen: "img/quesoCrema280.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Queso blanco",
+        descripcion: "especificar en el pedido",
+        precio: 3000,
+        imagen: "img/quesoBlanco.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Quesos untables",
+        descripcion: "especificar en el pedido",
+        precio: 1750,
+        imagen: "img/untables.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Yogur natural 140",
+        descripcion: "especificar en el pedido",
+        precio: 1250,
+        imagen: "img/natural140.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Yogur natural 280",
+        descripcion: "especificar en el pedido",
+        precio: 2350,
+        imagen: "img/natural280.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Queso mascarpone",
+        precio: 2400,
+        imagen: "img/mascarpone.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Dulce de leche 200",
+        precio: 1450,
+        imagen: "img/dulce200.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Dulce de leche 400",
+        precio: 2510,
+        imagen: "img/dulce400.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Ricota",
+        descripcion: "especificar en el pedido",
+        precio: 2400,
+        imagen: "img/ricota.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Yogur cremoso 125",
+        descripcion: "especificar pedidos",
+        precio: 600,
+        imagen: "img/yogur125.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Yogur con frutas",
+        descripcion: "Enteros:Ananá,mango,durazno,frutilla,cereza,arándano <br> Descremados: durazno,frutilla,arándano",
+        precio: 1450,
+        imagen: "img/yogurFrutas.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Yogur Firme",
+        descripcion: "Frutilla y Vainilla <br>Especificar en el pedido.",
+        precio: 920,
+        imagen: "img/Firme.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Yogur TOPS",
+        descripcion: "Cereal y Granola. <br>Especificar en el pedido.",
+        precio: 1200,
+        imagen: "img/YogurTOPS.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Yogur bebible entero",
+        descripcion: "Especificar en el pedido: frutilla,vainilla,durazno,arándano.",
+        precio: 1800,
+        imagen: "img/bebible.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Yogur bebible descremado",
+        descripcion: "Especificar en el pedido: frutilla, vainilla, durazno.",
+        precio: 1800,
+        imagen: "img/bebibleDesc.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Crema Chantilly",
+        descripcion: "pedido con anticipación",
+        precio: 7300,
+        imagen: "img/chantilly.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Manteca 100gr",
+        precio: 1600,
+        imagen: "img/manteca.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Queso rallado 120gr",
+        precio: 3690,
+        imagen: "img/rallado120.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Queso rallado 40gr",
+        descripcion: "20 unidades",
+        precio: 25750,
+        imagen: "img/rallado40.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Queso cremoso",
+        descripcion: "consultar precio por caja",
+        precio: 9950,
+        imagen: "img/cremoso.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Queso barra",
+        precio: 12750,
+        imagen: "img/danbo.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Queso fraccionado",
+        descripcion: "por salut sin sal. Venta por kilo",
+        precio: 13850,
+        imagen: "img/salutSinSal.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Queso fraccionado",
+        descripcion: "por salut light. Venta por kilo",
+        precio: 9684,
+        imagen: "img/salut.png",
+        categoria: "Lácteos Tregar"
+    },
+    {
+        nombre: "Papas clásicas 65g",
+        descripcion: "la caja trae 25 unidades.",
+        precio: 900,
+        imagen: "img/papaClasica.png",
+        categoria: "Chil Slices: El nombre del snack!"
+    },
+    {
+        nombre: "Papas clásicas 150g",
+        descripcion: "la caja trae 15 unidades.",
+        precio: 1550,
+        imagen: "img/papaClasica.png",
+        categoria: "Chil Slices: El nombre del snack!"
+    },
+    {
+        nombre: "Papas corte americano",
+        descripcion: "la caja trae 25 unidades.",
+        precio: 900,
+        imagen: "img/papaAmericano.png",
+        categoria: "Chil Slices: El nombre del snack!"
+    },
+    {
+        nombre: "Papas sabor ketchup",
+        descripcion: "la caja trae 25 unidades.",
+        precio: 1150,
+        imagen: "img/papaKetchup.png",
+        categoria: "Chil Slices: El nombre del snack!"
+    },
+    {
+        nombre: "Papas sabor Jamón Serrano",
+        descripcion: "la caja trae 25 unidades.",
+        precio: 1150,
+        imagen: "img/papaJamon.png",
+        categoria: "Chil Slices: El nombre del snack!"
+    },
+    {
+        nombre: "Papas sabor Salame de campo",
+        descripcion: "la caja trae 25 unidades.",
+        precio: 1150,
+        imagen: "img/papaSalame.png",
+        categoria: "Chil Slices: El nombre del snack!"
+    },
+    {
+        nombre: "Papas Pay",
+        descripcion: "140g",
+        precio: 1500,
+        imagen: "img/papaKetchup.png",
+        categoria: "Chil Slices: El nombre del snack!"
+    },
+    {
+        nombre: "Papas tubo",
+        descripcion: "Especificar en el pedido: clásica, crema y cebolla, barbacoa.",
+        precio: 3700,
+        imagen: "img/papasTubo.png",
+        categoria: "Chil Slices: El nombre del snack!"
+    },
+    {
+        nombre: "Conitos 4G",
+        descripcion: "Especificar en el pedido: sabor pizza y queso",
+        precio: 1100,
+        imagen: "img/conitos.png",
+        categoria: "Chil Slices: El nombre del snack!"
+    },
+    {
+        nombre: "LEONACHOS",
+        precio: 1100,
+        imagen: "img/leonachos.png",
+        categoria: "Chil Slices: El nombre del snack!"
+    },
+    {
+        nombre: "Palito tradicional",
+        descripcion: "120g",
+        precio: 700,
+        imagen: "img/palitoTradicional.png",
+        categoria: "Chil Slices: El nombre del snack!"
+    },
+    {
+        nombre: "Palito sabor Cheddar",
+        descripcion: "120g",
+        precio: 900,
+        imagen: "img/palitoCheddar.png",
+        categoria: "Chil Slices: El nombre del snack!"
+    },
+    {
+        nombre: "Palito sabor Jamón serrano",
+        descripcion: "120g",
+        precio: 900,
+        imagen: "img/palitoJamon.png",
+        categoria: "Chil Slices: El nombre del snack!"
+    },
+    {
+        nombre: "Salchichas OTTO",
+        descripcion: "por unidad",
+        precio: 1000,
+        imagen: "img/salchichas.png",
+        categoria: "Fiambres: Tremblay y Recreo"
+    },
+    {
+        nombre: "Salchichas OTTO",
+        descripcion: "por caja",
+        precio: 920,
+        imagen: "img/salchichas.png",
+        categoria: "Fiambres: Tremblay y Recreo"
+    },
+    {
+        nombre: "Fiambre cocido de pata de cerdo",
+        descripcion: "Venta por kilo",
+        precio: 7700,
+        imagen: "img/fiambre.png",
+        categoria: "Fiambres: Tremblay y Recreo"
+    },
+    {
+        nombre: "Salame milan",
+        descripcion: "Venta por kilo",
+        precio: 14000,
+        imagen: "img/milan.png",
+        categoria: "Fiambres: Tremblay y Recreo"
+    },
+    {
+        nombre: "Salamin picado grueso",
+        descripcion: "Venta por kilo",
+        precio: 14500,
+        imagen: "img/salamin.png",
+        categoria: "Fiambres: Tremblay y Recreo"
+    },
+    {
+        nombre: "Queso Mozzarella",
+        descripcion: "Venta por kilo",
+        precio: 10,
+        imagen: "img/mozza.png",
+        categoria: "Fiambres: Tremblay y Recreo"
+    },
+
+];
 let carrito = [];
 
 //FUNCION RENDER
